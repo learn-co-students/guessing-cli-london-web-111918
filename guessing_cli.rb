6c83037f9526b198dda7f871bb0e251b82c622e7
IMH51 <ianmartinharrison@outlook.com>
2018-11-01 17:41:00 +0000

    all tests pass and game works correctly

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,21 @@
 # Code your solution here!
+require "pry"
+
+def run_guessing_game
+  loop do
+  puts "Guess a number between 1 and 6."
+  user_input = gets.chomp
+  if user_input.downcase == 'exit'
+    break
+  end
+  computer_input = rand(1..6).to_s
+  if !(1..6).include?(user_input.to_i)
+    puts "Invalid Input."
+  elsif user_input == computer_input
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{computer_input}."
+  end
+  end
+  puts "Goodbye!"
+end
